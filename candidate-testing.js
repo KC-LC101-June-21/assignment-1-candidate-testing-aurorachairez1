4b3a8b0863338490d3baffc2438762d9b67d6da6
AuroraChairez <>
2021-06-06 21:35:42 +0000

    completed all steps

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,10 +4,13 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+
+
+let question = "Who was the first American woman in space?  "
+let correctAnswer = "Sally Ride"
+let candidateAnswer; 
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,11 +18,14 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("Please enter your name:");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+
+candidateAnswer = input.question(question)
+
 
 
 }
@@ -27,7 +33,11 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if(candidateAnswer === correctAnswer){
+  console.log("The answer is correct");
+} else if(candidateAnswer != correctAnswer){
+  console.log("That is NOT correct, the correct answer is " + correctAnswer);
+}
 
   let grade;
   
@@ -38,6 +48,8 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
+
+  console.log("Hello, " + candidateName + "!");
   
   askQuestion();
   gradeQuiz(this.candidateAnswers);
